355ca46581005a4d440615bdcda78760b23c6186
Brody Davison <brodydavison4@gmail.com>
2021-04-27 21:51:21 -0400

    Pause Screen
    Created an extremely basic pause menu. Escape key pulls up the menu. Switches between menu_open & menu_close tracks for opening and closing with escape, and menu_confirm activates on button clicks.

---
+++ Assets/Scripts/PauseScreen.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseScreen : MonoBehaviour
+{
+    FMODUnity.StudioEventEmitter[] emitters;
+    [SerializeField] Canvas canvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        emitters = transform.GetComponents<FMODUnity.StudioEventEmitter>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            canvas.gameObject.SetActive(!canvas.gameObject.activeSelf);
+
+            if (canvas.gameObject.activeSelf)
+            {
+                emitters[1].Play();
+            }
+            else
+            {
+                emitters[2].Play();
+            }
+        }
+    }
+
+    public void continueButton()
+    {
+        emitters[0].Play();
+        canvas.gameObject.SetActive(false);
+    }
+
+    public void exitButton()
+    {
+        emitters[0].Play();
+        Application.Quit();
+    }
+}
