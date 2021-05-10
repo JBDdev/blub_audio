f2258e85b830f4c2cf10f245193397521388734d
Brody Davison <brodydavison4@gmail.com>
2021-05-10 06:53:24 -0400

    Pulse speaker parameterization
    Uses the distance between the object and the player to adjust sound volume via distance formula squared for optimization's sake

--- Assets/Scripts/SpeakerAudio.cs
+++ Assets/Scripts/SpeakerAudio.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class SpeakerAudio : MonoBehaviour
 {
+    [SerializeField] Transform player;
+    FMODUnity.StudioEventEmitter e;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        e = transform.GetComponent<FMODUnity.StudioEventEmitter>();    
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float distanceSq = Mathf.Pow(transform.position.x - player.position.x, 2.0f) + Mathf.Pow(transform.position.y - player.position.y, 2.0f);
+        e.SetParameter("Distance", distanceSq / 200f);
     }
 }
